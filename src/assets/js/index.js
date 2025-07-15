60a955f1ccf222d6cf196ebb4cb0e962929217d9
Elman <elmani3107@gmail.com>
2025-07-16 00:44:06 +0200

    update
    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,5 @@
 /**
- * @author Luuxis
+ * @author SentryXSystems
  * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
  */
 
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "SentryXSystems" },
+            { "message": "Salut je suis du code.", "author": "SentryXSystems" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "SentryXSystems" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
